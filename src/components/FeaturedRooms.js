c6836ff0a6dc0315169306c0f900374c45794ab0
Mritunjoy <mritunjoy.mahanta001@gmail.com>
2020-12-21 23:18:53 +0530

    Created loading component and the basic structure of Room component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     const { featuredRooms: rooms } = this.context;
     console.log(rooms);
-    return <div>Featured Rooms</div>;
+    return (
+      <div>
+        Featured Rooms
+        <Room />
+        <Loading />
+      </div>
+    );
   }
 }
