bf23f16b5c6a7381e4f46e1470cf6ba17064e5be
Mritunjoy <mritunjoy.mahanta001@gmail.com>
2020-12-22 20:07:28 +0530

    completed the single room page setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ScrollTop from "./components/ScroollTop";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
       </Switch>
+      <ScrollTop />
     </>
   );
 }
